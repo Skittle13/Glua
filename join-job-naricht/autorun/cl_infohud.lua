65135409eb15b7966eb375c3a0b6c26d70dba2bf
Skittle13 <129594612+Skittle13@users.noreply.github.com>
2023-05-16 18:10:41 +0200

    Update cl_infohud.lua

--- join-job-naricht/autorun/cl_infohud.lua
+++ join-job-naricht/autorun/cl_infohud.lua
@@ -4,26 +4,30 @@ local scrh = ScrH()
 local x = scrw / 2
 local y = scrh * 0.09
 
-surface.CreateFont("InfoHudFont", {font = "Roboto", size = 20, weight = 800})
+local g = scrw / 2
+local h = scrh * 0.07
+
+surface.CreateFont("STG_InfoHudFont", {font = "Roboto", size = 20, weight = 800})
 
 local ply, default_team
 
 timer.Simple(0, function()
-	default_team = GAMEMODE.DefaultTeam  -- Beitritsberuf Team
+	default_team = GAMEMODE.DefaultTeam
 end)
 
 local function DrawJoinJob()
 	ply = ply or LocalPlayer()
 	if ply:Team() != default_team then return end
-	draw.SimpleTextOutlined("Du bist im Anfangsjob, drücke F4, um den Beruf zu wechseln.", "InfoHudFont", x, y, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, color_black)
+	draw.SimpleTextOutlined("drücke F4, um deinen Beruf zu wechseln.", "STG_InfoHudFont", x, y, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, color_black)
+        draw.SimpleTextOutlined("Du bist im Anfangsjob", "STG_InfoHudFont", g, h, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, color_black)
 end
-hook.Add("HUDPaint", "NG_JoinJob", DrawJoinJob)
+hook.Add("HUDPaint", "STG_JoinJob", DrawJoinJob)
 
 local function DrawCSSMount()
 	if !IsMounted("cstrike") and !util.IsValidModel("models/props/cs_assault/money.mdl") then
-		draw.SimpleTextOutlined("Der Server benutzt Objekte aus CSS, installiere es, um ohne Fehler spielen zu können.", "InfoHudFont", x, y + 35, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, color_black)
+		draw.SimpleTextOutlined("Der Server benutzt Objekte aus CSS, installiere es, um ohne Fehler spielen zu können.", "STG_InfoHudFont", x, y + 35, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, color_black)
 	else
-		hook.Remove("HUDPaint", "NG_CSSMount")
+		hook.Remove("HUDPaint", "STG_CSSMount")
 	end
 end
-hook.Add("HUDPaint", "NG_CSSMount", DrawCSSMount)
+hook.Add("HUDPaint", "STG_CSSMount", DrawCSSMount)
